eb8bff30fee79c49f4d281549860bcf8f1b9785d
Ticiano Licarzze <ticilicarzze@gmail.com>
2023-04-16 23:25:13 -0300

    BaSP-W05-2023: finish exercise 3

--- Week-05/exercises/02-strings.js
+++ Week-05/exercises/02-strings.js
@@ -1,34 +1,34 @@
 console.log('--Exercise 2: STRINGS');
 
-// Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
+// a. Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
 
 var str1 = 'abcdabcdabcdabcd';
 console.log(str1.toUpperCase());
 
-// Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+// b. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
 
 var str2 = 'aaaabbbbcccc';
 console.log(str2.substring(0, 5));
 
-// Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).
+// c. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).
 
 var srt3 = '123456789abc';
 var srt4 = srt3.substring(9);
 console.log(srt4);
 
-// Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+// d. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
 
 var str5 = 'aaaAABBbbb';
 var str6 = str5.substring(0, 1);
 var str7 = str5.substring(1);
 console.log(str6.toUpperCase() + str7.toLowerCase());
 
-// Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
+// e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
 
 var str8 = 'Hello world'
 console.log(str8.indexOf(' '));
 
-// Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
+// f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
 
 var str10 = 'respoNsive desigN'
 var blankSpace = str10.indexOf(' ')
--- Week-05/exercises/03-arrays.js
+++ Week-05/exercises/03-arrays.js
@@ -1,1 +1,39 @@
 console.log('--Exercise 3: ARRAYS');
+
+// a. Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+console.log(months[4])
+console.log(months[10])
+
+// b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+
+console.log(months.sort())
+
+// c. Agregar un elemento al principio y al final del array (utilizar unshift y push).
+
+months.unshift("principio")
+months.push("final")
+console.log(months)
+
+// d. Quitar un elemento del principio y del final del array (utilizar shift y pop).
+
+months.shift()
+months.pop()
+console.log(months)
+
+// e. Invertir el orden del array (utilizar reverse).
+
+months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+console.log(months.reverse())
+
+// f. Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
+
+months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+console.log(months.join("-"))
+
+// g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
+
+months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+console.log(months.slice(4, 11))
